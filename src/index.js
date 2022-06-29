63060d60ece979d249baf9944ef5f95fac09ac56
Afolabi Akorede <brainconnect0@gmail.com>
2022-06-29 20:40:49 +0100

    display hamburger and the logo image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './style.css';
-import Displayovies from './modules/app.js';
+import DisplayMovies from './modules/app.js';
+import logo from './back-img-removebg.png';
 
 window.addEventListener('DOMContentLoaded', () => {
-  Displayovies.initialize();
+  DisplayMovies.initialize();
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-items');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
+
+var backImg= document.getElementById('head-img');
+backImg.src = logo;
+//document.body.appendChild(backImg);
